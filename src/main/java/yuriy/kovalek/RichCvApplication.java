24b4ad065caadaceaf1d1d519a997995735bde02
Yuriy Kovalek <yuriy.kovalek@gmail.com>
2016-03-23 22:58:10 +0200

    Spring Initlzr project for starters

---
+++ src/main/java/yuriy/kovalek/RichCvApplication.java
@@ -0,0 +1,12 @@
+package yuriy.kovalek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichCvApplication.class, args);
+	}
+}
---
+++ src/test/java/yuriy/kovalek/RichCvApplicationTests.java
@@ -0,0 +1,18 @@
+package yuriy.kovalek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RichCvApplication.class)
+@WebAppConfiguration
+public class RichCvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
