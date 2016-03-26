260733c62472b0b27eb786e9360794c436a8c244
Yuriy Kovalek <yuriy.kovalek@gmail.com>
2016-03-26 05:00:50 +0200

    Removed example jsp with GDocs-generated CV. JSTL views not rendering.

--- src/main/java/yuriy/kovalek/RichCvApplication.java
+++ src/main/java/yuriy/kovalek/RichCvApplication.java
@@ -1,5 +1,6 @@
 package yuriy.kovalek;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -13,12 +14,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import javax.servlet.ServletContext;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class RichCvApplication extends WebMvcConfigurerAdapter {
+	@Autowired
+	ServletContext context;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RichCvApplication.class, args);
@@ -27,10 +33,11 @@ public class RichCvApplication extends WebMvcConfigurerAdapter {
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setServletContext(context);
+		viewResolver.setViewClass(JstlView.class);
 		viewResolver.setPrefix("/WEB-INF/views/jsp/");
-//		viewResolver.setSuffix(".jsp");
-		viewResolver.setSuffix(".html");
+		viewResolver.setSuffix(".jsp");
+//		viewResolver.setSuffix(".html");
 		return viewResolver;
 	}
 
@@ -39,7 +46,13 @@ public class RichCvApplication extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 
+	@Bean
+	public DefaultServletHandlerConfigurer configurer() {
+		return new DefaultServletHandlerConfigurer(context);
+	}
+
 	@Override
+	@Autowired
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 //		super.configureDefaultServletHandling(configurer);
 ////		configurer.enable("org.springframework.web.servlet.DispatcherServlet");
