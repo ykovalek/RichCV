8f93dcde2a6d2baaa1d9eb9f236f8e6f7cc98b79
Yuriy Kovalek <yuriy.kovalek@gmail.com>
2016-04-02 19:05:58 +0300

    Fixed tests.

--- src/test/java/yuriy/kovalek/RootControllerTests.java
+++ src/test/java/yuriy/kovalek/RootControllerTests.java
@@ -2,8 +2,12 @@ package yuriy.kovalek;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -15,6 +19,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * Created by Yuriy Kovalek on 4/2/16.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RichCvApplication.class)
+@WebAppConfiguration
 public class RootControllerTests {
     @Autowired
     WebApplicationContext context;
